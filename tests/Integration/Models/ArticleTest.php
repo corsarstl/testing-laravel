a8619e419b53cb6b124e5238ba3bc56fe7404b30
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-02-26 17:06:48 +0200

    finish 'Testing Eloquent'

---
+++ tests/Integration/Models/ArticleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Integraton\Models;
+
+use App\Article;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+class ArticleTest extends TestCase
+{
+//    use DatabaseTransactions;
+
+    /** @test */
+    public function itFetchesTrendingArticles()
+    {
+        factory(Article::class, 2)->create();
+        factory(Article::class)->create(['reads' => 10]);
+        $mostPopular = factory(Article::class)->create(['reads' => 20]);
+
+        $articles = Article::trending();
+
+        $this->assertEquals($mostPopular->id, $articles->first()->id);
+        $this->assertCount(3, $articles);
+    }
+}
